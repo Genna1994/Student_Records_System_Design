a94197ebd3b4c944b5d6b18307d3d68052c782a8
Genna1994 <dgenna777@gmail.com>
2026-05-31 12:03:50 +0300

    connection.ts

--- api/queries/connection.ts
+++ api/queries/connection.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 import { env } from "../lib/env";
-import * as schema from "@db/schema";
-import * as relations from "@db/relations";
+import * as schema from "../../db/schema";
+import * as relations from "../../db/relations";
 
 const fullSchema = { ...schema, ...relations };
 
@@ -9,9 +10,15 @@ let instance: ReturnType<typeof drizzle<typeof fullSchema>>;
 
 export function getDb() {
   if (!instance) {
-    instance = drizzle(env.databaseUrl, {
-      mode: "planetscale",
+    const pool = mysql.createPool({
+      uri: env.databaseUrl,
+      ssl: { rejectUnauthorized: false },
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
+    instance = drizzle(pool, {
       schema: fullSchema,
+      mode: "default",
     });
   }
   return instance;
